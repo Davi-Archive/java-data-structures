91081f44cabe125e61576418b7e9d0d175ea4a4e
d1av <davi4alves@gmail.com>
2023-01-19 18:29:07 -0300

    feat: Indirect Recursion

---
+++ data001/IndirectRecursion.java
@@ -0,0 +1,24 @@
+package data001;
+
+public class IndirectRecursion {
+
+
+    public static void main(String[] args) {
+        funA(20);
+    }
+
+    public static void funB(int n) {
+        if (n > 1) {
+            System.out.println(n);
+            funA(n / 2);
+        }
+    }
+
+    public static void funA(int n) {
+        if (n > 0) {
+            System.out.println(n);
+            funB(n - 1);
+        }
+    }
+
+}
