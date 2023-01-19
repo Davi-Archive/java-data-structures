6af7547bc29a5c2b057557cc636720e63b32b21c
d1av <davi4alves@gmail.com>
2023-01-19 19:03:51 -0300

    feat: factorial number

---
+++ data001/recursion/FactorialNumber.java
@@ -0,0 +1,26 @@
+package data001.recursion;
+
+public class FactorialNumber {
+
+    static int factorial(int n) {
+        if (n == 0 || n < 0) return 1;
+        System.out.println("fun " + n + " x " + (n - 1));
+        return factorial(n - 1) * n;
+    }
+
+    static int Ifactorial(int n) {
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+            System.out.println("loop factorial "+f);
+        }
+        return f;
+    }
+
+    public static void main(String[] args) {
+        int r = factorial(5);
+        System.out.println("RESULT REcur" + r);
+        int r2 = Ifactorial(5);
+        System.out.println("RESULT Loop" + r2);
+    }
+}
