86f28b4beaf5b08ab208bd1a4efddc465e372bdd
d1av <davi4alves@gmail.com>
2023-01-29 01:01:55 -0300

    feat: sort array

--- array/ArraySorting.java
+++ array/ArraySorting.java
@@ -15,12 +15,64 @@ public class ArraySorting {
         return reversedArr;
     }
 
+    private static int[] reverseWithTemp() {
+        int[] reversedArr = new int[arr.length];
+        int temp;
+        for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
+            temp = arr[i];
+            reversedArr[i] = arr[j];
+            reversedArr[j] = temp;
+        }
+        return reversedArr;
+    }
 
+    public static char[] leftShift(int numberOfChanges) {
+        char temp, temp2;
+        char[] newArr = {'a', 'b', 'c', 'd', 'e', 'f'};
+        for (int j = 0; j < numberOfChanges; j++) {
+            temp = newArr[0];
+            for (int i = 0; i < newArr.length - 1; i++) {
+                newArr[i] = newArr[i + 1];
+            }
+
+            newArr[newArr.length - 1] = temp;
+        }
+        return newArr;
+    }
+
+    public static int[] sortArray() {
+        int[] array = {4, 5, 6, 8, 2, 3,45,34,12,56};
+        boolean sorted = false;
+        int temp;
+        while (!sorted) {
+            sorted=true;
+            for (int i = 0; i < array.length-1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    sorted=false;
+                }
+            }
+        }
+        return array;
+    }
 
     public static void main(String[] args) {
         System.out.printf("Inverted Array");
         Arrays.stream(reverse()).forEach(x -> System.out.printf(" " + x));
 
+        System.out.println(" ");
+        System.out.printf("Reversed arr with temp var");
+        Arrays.stream(reverseWithTemp()).forEach(x -> System.out.printf(" " + x));
+
+        System.out.printf("%n Reversed char arr=>");
+        for (char character : leftShift(2)) {
+            System.out.printf(" " + character);
+        }
+
+        System.out.printf("%n Order array=>");
+        Arrays.stream(sortArray()).forEach(x -> System.out.printf(" " + x));
 
     }
 }
