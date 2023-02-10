f265505f7fdbd238f1c57cbee440b37b98c36659
d1av <davi4alves@gmail.com>
2023-02-10 19:23:37 -0300

    feat: :sparkles: add lower triangular matrix

---
+++ matrix/LowerTriangular.java
@@ -0,0 +1,17 @@
+package matrix;
+
+public class LowerTriangular {
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < 5; j++) {
+                if (i >= j) {
+                    matrix[i][j] = i + j + 1;
+                }
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
+}
